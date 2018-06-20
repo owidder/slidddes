a5d66cef96cd7dbb5404b2a9b024bea36c6851ff
Oliver Widder <oliver.widder@gmail.com>
2018-06-20 16:16:00 +0200

    remove simplAR

--- src/index.js
+++ src/index.js
@@ -6,13 +6,11 @@ import {initSlides} from './slides/slidesIndex';
 import * as slowSnakes from "./slides/slowSnakes/slowSnakes";
 
 import {slidAR} from './slides/slidAR/slidAR';
-import {simplAR} from './simplAR/simplAR';
 import {slidarGlobal} from './slides/slidAR/slidarGlobal';
 
 import './slidddes.css';
 
 window.slidAR = slidAR;
-window.simplAR = simplAR;
 
 slidarGlobal.slidesFolder = "slides/slowSnakes/html/";
 initSlides("#container", slowSnakes.init);
